d9edbd451b91491931aeeea14d15125f79600d0f
Anatoly Belov <scsi9x@gmail.com>
2017-04-20 16:25:58 +0300

    Update Constants.java

--- app/src/main/java/com/example/admin3/mobtransyandex/Constants.java
+++ app/src/main/java/com/example/admin3/mobtransyandex/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final String Yandex_getLangs  = "api/v1.5/tr.json/getLangs?";
 
-    public static final String Yandex_key = "key=trnsl.1.1.20151028T192402Z.8f268e719ad5e969.7407deec114ec4a184aa689ec39e43de96570bd7";
+    public static final String Yandex_key = "Put your key here";
 
     public static final int DB_VERSION = 7;
 
