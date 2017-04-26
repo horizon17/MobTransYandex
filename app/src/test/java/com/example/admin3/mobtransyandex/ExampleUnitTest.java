8ea632620ef7fc13160894eff713dd61433925a4
Anatoly Belov <scsi9x@gmail.com>
2017-04-27 00:37:59 +0300

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/admin3/mobtransyandex/ExampleUnitTest.java
+++ app/src/test/java/com/example/admin3/mobtransyandex/ExampleUnitTest.java
@@ -1,11 +1,9 @@
 package com.example.admin3.mobtransyandex;
 
-import android.test.ActivityInstrumentationTestCase;
+import android.support.v7.app.AppCompatActivity;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,17 +11,36 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 //public class ExampleUnitTest extends ActivityInstrumentationTestCase<MainActivity> {
-public class ExampleUnitTest {
+public class ExampleUnitTest extends AppCompatActivity {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        //assertEquals(4, 2 + 2);
+
+//        Translator translator=new Translator(this);
+//        DataItem dataItem=new DataItem();
+//        dataItem.setInput("dog");
+//        dataItem.setDirs("en-ru");
+        // следующая строка вызывает ошибку android.util.Log.isLoggable(Ljava/lang/String;I)Z
+        // причина которой в том что volley не подходит для JUnit test,
+        // поскольку: isLoggable is a native method so its not available at runtime.
+        // Здень нужен RobolectricTestRunner, на который, к сожалению не хватило времени.
+        /*translator.getTranslation1(dataItem,new Translator.VolleyCallback1() {
+            @Override
+            public void onSuccess(String result) {
+                Log.v("ExampleUnitTest result: "+result);
+            }
+        });*/
     }
+
+
 
     @Before
     public void setUp() throws Exception {
 
 //        super.setU();
-//        activity = getActivit();
+        //MainActivity mainActivity=new MainActivity();
+
+        //Method method = mainActivity.get(methodName, argClasses);
         // arrange
 
         // act
