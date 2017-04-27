acdd67fd42e25cfc9e5685fbf9dead109c1e89ef
Anatoly Belov <scsi9x@gmail.com>
2017-04-27 08:38:02 +0300

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/admin3/mobtransyandex/ExampleUnitTest.java
+++ app/src/test/java/com/example/admin3/mobtransyandex/ExampleUnitTest.java
@@ -11,18 +11,24 @@ import org.junit.Test;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 //public class ExampleUnitTest extends ActivityInstrumentationTestCase<MainActivity> {
-public class ExampleUnitTest extends AppCompatActivity {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        //assertEquals(4, 2 + 2);
+public class ExampleUnitTest{
 
-//        Translator translator=new Translator(this);
-//        DataItem dataItem=new DataItem();
-//        dataItem.setInput("dog");
-//        dataItem.setDirs("en-ru");
+    @Test
+    public void translator_isCorrect() throws Exception {
+
+        MainActivity mainActivity=new MainActivity();
+
+        // в момент создания queue = Volley.newRequestQueue(mContext); происходит java.lang.reflect.InvocationTargetException,
+        // похоже дело в JUnit test:
+        //Translator translator=new Translator(mainActivity.getContext());
+
+        DataItem dataItem=new DataItem();
+        dataItem.setInput("dog");
+        dataItem.setDirs("en-ru");
+
         // следующая строка вызывает ошибку android.util.Log.isLoggable(Ljava/lang/String;I)Z
         // причина которой в том что volley не подходит для JUnit test,
-        // поскольку: "isLoggable is a native method so its not available at runtime."
+        // поскольку: "isLoggable is a native method so its not available at runtime".
         // Здень нужен RobolectricTestRunner, на который, к сожалению не хватило времени.
         /*translator.getTranslation(dataItem,new Translator.VolleyCallback() {
             @Override
